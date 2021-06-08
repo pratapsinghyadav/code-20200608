28da6bd7d645bbe1ce4015666ff088f79b0692b6
Pratap <pratapegg2011@hotmail.com>
2021-06-09 00:02:40 +0530

    Home Page Design
    Added Log Message about server running
    Added Form to home page to take input for Height and weight
    Added Post Route in index route to accept Request from home page.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to BMI Calculator.' });
+
+});
+router.post("/", function(req, res){
+  var weight= Number(req.body.weight);
+  var height = Number(req.body.height);
+  height = height/100;
+  var result = (weight / (height*height));
+
+  res.send("Result: " + result.toFixed(2));
 });
 
 module.exports = router;
