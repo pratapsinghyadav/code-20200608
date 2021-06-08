6a8e1527cb54f79a91fbd1410d40488727c43935
Pratap <pratapegg2011@hotmail.com>
2021-06-09 00:28:50 +0530

    Added route for bulk BMI Calculator
    Increase Json Parser Memory size
    Added Async Package
    Added POST /calculate_bmi

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '100mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,49 @@ router.post("/", function(req, res){
   res.send("Result: " + result.toFixed(2));
 });
 
+/* GET BMI Bulk Calculating. */
+router.post('/calculate_bmi', function(req, res, next) {
+  var async = require('async'), asyncTasks = [];
+
+
+  var requestData = req.body;
+  asyncTasks.push(function (asyncMainCb) {
+    async.map(
+        requestData,
+        function (transaction, asyncCb) {
+
+          var weight= Number(transaction.WeightKg);
+          var height = Number(transaction.HeightCm);
+          height = height/100;
+          var bmi = (weight / (height*height)).toFixed(2);
+          var risk = "Malnutrition risk"
+
+          if(bmi>=18.5 && bmi<=24.9) {
+            risk = "Low risk";
+          }else if(bmi>=25 && bmi<=29.9) {
+            risk = "Enhanced risk";
+          }else if(bmi>=30 && bmi<=34.9) {
+            risk = "Medium risk";
+          }else if(bmi>=35 && bmi<=39.9) {
+            risk = "High risk";
+          }else if(bmi>=40) {
+            risk = "Very high risk";
+          }
+
+          transaction.bmi = bmi;
+          transaction.risk = risk;
+          return asyncCb(null, transaction);
+        },
+        function (error, result) {
+          if(error)   console.log(error);
+          return asyncMainCb(null, result);
+        });
+  });
+
+  async.series(asyncTasks, function (mainError, mainResult) {
+    return res.send({success: true, data: mainResult[0]});
+  });
+});
+
+
 module.exports = router;
